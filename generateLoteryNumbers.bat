9b5ad24ceff8750c8665a28cbcfd38797a3c750e
leiltonramos <67462591+leiltonramos@users.noreply.github.com>
2021-09-13 15:35:47 -0300

    Add files via upload

---
+++ generateLoteryNumbers.bat
@@ -0,0 +1,47 @@
+@echo off
+
+set /a num1=%random% %%25 +1
+set /a num2=%random% %%25 +1
+set /a num3=%random% %%25 +1
+set /a num4=%random% %%25 +1
+set /a num5=%random% %%25 +1
+set /a num6=%random% %%25 +1
+set /a num7=%random% %%25 +1
+set /a num8=%random% %%25 +1
+set /a num9=%random% %%25 +1
+set /a num10=%random% %%25 +1
+set /a num11=%random% %%25 +1
+set /a num12=%random% %%25 +1
+set /a num13=%random% %%25 +1
+set /a num14=%random% %%25 +1
+set /a num15=%random% %%25 +1
+set /a num16=%random% %%25 +1
+set /a num17=%random% %%25 +1
+set /a num18=%random% %%25 +1
+set /a num19=%random% %%25 +1
+set /a num20=%random% %%25 +1
+set /a num21=%random% %%25 +1
+
+echo %num1%
+echo %num2%
+echo %num3%
+echo %num4%
+echo %num5%
+echo %num6%
+echo %num7%
+echo %num8%
+echo %num9%
+echo %num10%
+echo %num11%
+echo %num12%
+echo %num13%
+echo %num14%
+echo %num15%
+echo %num16%
+echo %num17%
+echo %num18%
+echo %num19%
+echo %num20%
+echo %num21%
+
+pause
